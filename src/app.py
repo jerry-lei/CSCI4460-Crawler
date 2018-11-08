430a366971d6945b67701e89cefb83dae9c5436a
Daniel McCrevan <danielmccrevan@gmail.com>
2018-11-08 15:41:10 -0500

    Linted the server file

--- src/app.py
+++ src/app.py
@@ -1,32 +1,53 @@
+"""This is the server module.
+
+This starts up the restful web service that will direct
+requests to specific components.
+
+"""
+
+import ast
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from crawl.scheduler import Scheduler
-import time
-import ast
 
-app = Flask(__name__)
-api = Api(app)
+APP = Flask(__name__)
+API = Api(APP)
 
 class DomainCrawl(Resource):
-    def post(self):
+    """The crawl API endpoint process URLs
+
+    Available methods:
+        - POST
+    """
+
+    @staticmethod
+    def post():
+        """The POST endpoint that crawls & processes links
+
+        Payload:
+            [JSON] -- JSON Object containing a URLS key that contains a list of URLs
+
+        Returns:
+            [JSON] -- JSON Object containing a list of bad URLs
+        """
         parser = reqparse.RequestParser()
         parser.add_argument("URLS")
         args = parser.parse_args()
-        if args["URLS"] == None:
+        if args["URLS"] is None:
             return "Bad Request: No URLS key found", 500
         urls = ast.literal_eval(args["URLS"])
         payload = []
         for url in urls:
             payload.append(urls[url])
-        scheduler.dump_hp_links(payload)
-        scheduler.print_crawled_hp()
+        SCHEDULER.dump_hp_links(payload) # Send the URLs to the scheduler
+        SCHEDULER.print_crawled_hp()
         return "Success", 200
 
-api.add_resource(DomainCrawl, "/crawl")
+API.add_resource(DomainCrawl, "/crawl")
 
 # Start the scheduler thread
-scheduler = Scheduler(5)
-scheduler.start()
+SCHEDULER = Scheduler(5)
+SCHEDULER.start()
 
 # Start the server
-app.run(debug=True)
+APP.run(debug=True)
