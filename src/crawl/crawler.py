e20089ad31b7764d0829757dfb94b7e564a8c04c
Daniel McCrevan <danielmccrevan@gmail.com>
2018-11-08 20:58:45 -0500

    Added URL Validation & Changed return object

--- src/crawl/crawler.py
+++ src/crawl/crawler.py
@@ -1,22 +1,26 @@
 import urllib.request
+import validators
 from urllib.error import URLError, HTTPError, ContentTooShortError
 
 """ Source: https://docs.python.org/3/howto/urllib2.html ('Fetching URLs')
 """
+
 def crawl_link(link):
+    if not validators.url(link):
+        return (None, False, "Invalid URL")
     print("Currently handling link: " + link)
     my_link = link
     req = urllib.request.Request(my_link)
     try:
         response = urllib.request.urlopen(req)
     except HTTPError as e:
-        return (None, e.code)
-    except URLError as e:
-        return (None, e.reason)
+        return (None, False, e.reason)
     except ContentTooShortError as e:
-        return (None, "ContentTooShortError")
+        return (None, False, e.reason)
+    except URLError as e:
+        return (None, False, e.reason)
     else:
-        return (response, "Success")
+        return (response, True, "")
 
 
 """ GOOD TEST CASES:
